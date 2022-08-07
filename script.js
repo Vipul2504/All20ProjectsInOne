d11f5b7027de657f0b40458a100e906a43dcaf01
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2022-08-07 17:25:08 +0530

    Javascript 20 projects

---
+++ script.js
@@ -0,0 +1,123 @@
+const data = [
+    {
+        id:1, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:2, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:3, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:4, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:5, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:6, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:7, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:8, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:9, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:10, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:11, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:12, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:13, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:14, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:15, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:16, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:17, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:18, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:19, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    },
+    {
+        id:20, 
+        name: "movie-seat-booking",
+        link:"https://movieseat-booking-js.netlify.app/"
+    }
+];
+
+// const buttons = document.querySelectorAll('#btn')
+// buttons.forEach(btns =>{
+//     btns.addEventListener('click',()=> {
+//         console.log(btns.parentElement.parentElement.parentElement.id)
+//     })
+// })
+
+// const a = document.querySelectorAll('a');
+// a.forEach( ab => {
+//     ab.addEventListener('click', (e) =>{
+//         e.preventDefault()
+//     })
+// })
+
+
+
+// add link to the dom
+
+
+
